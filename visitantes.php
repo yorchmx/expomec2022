b422cc8f33acdb6bb7a5d871700e3c796a8e711d
Jorge Manrique <58120477+yorchmx@users.noreply.github.com>
2022-09-24 17:00:29 -0500

    Tabla

---
+++ visitantes.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <title>Visitantes</title>
+  </head>
+  <body>
+    <h2 class="text-center"><span class="badge badge-success">Visitantes Expomec 2022</span></h2>
+
+        <!-- Tabla -->
+        <div style="height: 50px;"></div>
+        <div class="container">
+            <h3>Tabla con <span class="badge badge-primary">HTML puro</span></h3>
+            <div class="row">
+                <div class="col-lg-12">
+                    <table border="1 cellpadding=1">
+                        <thead>
+                            <th>Nombre</th>
+                            <th>Email</th>
+                            <th>Celular</th>
+                            <th>Pagado</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Yorch Manrique</td>
+                                <td>guillmac@hotmail.com</td>
+                                <td>5548707478</td>
+                                <td>Sí</td>
+                            </tr>
+                            <tr>
+                                <td>Mac Manrique</td>
+                                <td>mac@hotmail.com</td>
+                                <td>5548707478</td>
+                                <td>Sí</td>
+                            </tr>
+                                <td>greta Manrique</td>
+                                <td>greta@hotmail.com</td>
+                                <td>5548707478</td>
+                                <td>Sí</td>
+                        </tbody>
+
+                    </table>
+
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
